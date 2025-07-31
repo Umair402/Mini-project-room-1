58e6455d282d6b5c89ad5b296e8cfa6e1b712a04
Abigail Carroll <abigailcarroll50@gmail.com>
2025-07-31 12:19:05 +0100

    fixed login page location

---
+++ src/test/java/com/sparta/room1/pages/LoginPage.java
@@ -0,0 +1,56 @@
+package com.sparta.room1.pages;
+
+import net.thucydides.core.pages.PageObject;
+import net.thucydides.core.pages.WebElementFacade;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@DefaultUrl("https://automationexercise.com/login")
+public class LoginPage extends PageObject {
+
+    @FindBy(xpath = "//*[@data-qa='login-email']")
+    private WebElementFacade emailFieldLogin;
+
+    @FindBy(xpath = "//*[@data-qa='password']")
+    private WebElementFacade passordField;
+
+    @FindBy(xpath = "//*[@data-qa='signup-name']")
+    private WebElementFacade nameField;
+
+    @FindBy(xpath = "//*[@data-qa='signup-email']")
+    private WebElementFacade emailFieldSignup;
+
+    @FindBy(xpath = "//*[@data-qa='login-button']")
+    private WebElementFacade loginButton;
+
+    @FindBy(xpath = "//*[@data-qa='signup-button']")
+    private WebElementFacade signupButton;
+
+
+
+    public void enterEmailLogin(String email)
+    {
+        emailFieldLogin.type(email);
+    }
+
+    public void enterPasswordLogin(String password)
+    {
+        passordField.type(password);
+    }
+
+    public void enterNameSignup(String name)
+    {
+        nameField.type(name);
+    }
+
+    public void enterEmailSignup(String email)
+    {
+        emailFieldSignup.type(email);
+    }
+
+
+
+
+}

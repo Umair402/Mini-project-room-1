f1888d3252e28933fe6bcf5c28271596aa058270
Umair402 <umair.qureshi@hotmail.co.uk>
2025-07-31 14:04:50 +0100

    first draft of inventory page

--- src/test/java/com/sparta/room1/pages/ProductsPage.java
+++ src/test/java/com/sparta/room1/pages/ProductsPage.java
@@ -2,6 +2,7 @@ package com.sparta.room1.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import net.serenitybdd.core.annotations.findby.FindBy;
@@ -9,21 +10,50 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 
+import java.util.List;
+
 
 @DefaultUrl("https://automationexercise.com/")
 public class ProductsPage extends PageObject {
 
-    @FindBy(css = "[data-product-id='1']")
-    private WebElementFacade blueTopAddToCartButton;
+    @FindBy(css = ".add-to-cart")
+    private List<WebElementFacade> AddToCartButtons;
+
+    @FindBy(css = ".choice")
+    private List<WebElementFacade> productDetailsButtons;
+
+    @FindBy(css = ".shop-menu ul li a")
+    private List<WebElementFacade> shopMenuButtons;
 
     @FindBy(className = "cart")
     private WebElementFacade ProductDetailsCartButton;
 
-    @FindBy(css = "[data-product-id='2']")
-    private WebElementFacade menTshirtAddToCartButton;
-
     @FindBy(tagName = "u")
-    private WebElementFacade goToCartButton;
+    private WebElementFacade goToCartAfterAddingButton;
+
+    @FindBy(id = "quantity")
+    private WebElementFacade quantityInput;
+
+    public void addToCartFromProductPage(int index){
+        AddToCartButtons.get(index).click();
+    }
+
+    public void goToProductDetailsPage(int index){
+        productDetailsButtons.get(index).click();
+    }
+
+    public void pressCartButton(){
+        shopMenuButtons.get(3).click();
+    }
+    
+    public void enterQuantityValue(String quantity){
+        quantityInput.type(quantity);
+    }
+
+    public void goToCartAfterAddingItem(){
+        goToCartAfterAddingButton.click();
+    }
+
 
 
 
